51098cc5ca84dd402bfb1d7102ac269d37a8276b
swyrik thupili <swyrik@GravityMacBookPro.local>
2024-04-06 19:46:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoPanelComponent } from "./todo-panel/todo-panel.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, TodoPanelComponent, SidebarComponent]
 })
 export class AppComponent {
   title = 'todo';
---
+++ src/app/todo-panel/todo-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-panel.component.html',
+  styleUrl: './todo-panel.component.scss'
+})
+export class TodoPanelComponent {
+
+}
