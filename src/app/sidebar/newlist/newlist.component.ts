2c7e2679b0581072878e3456a78138c8c5438653
swwyrik <swwyrik@gmail.com>
2024-04-21 02:05:59 +0530

    new task list code implemented

--- src/app/sidebar/newlist/newlist.component.ts
+++ src/app/sidebar/newlist/newlist.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { v4 as  uuidv4} from 'uuid';
 
 @Component({
   selector: 'app-newlist',
@@ -8,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './newlist.component.scss'
 })
 export class NewlistComponent {
+
+  toogleAddListFlag : boolean = false;
+  @ViewChild('newTaskList') newTaskList!: ElementRef<HTMLInputElement>;
+
+
+  constructor(private taskService: TaskService) {
+
+  }
+
+  @HostListener('window:keydown.esc', ['$event'])
+  handleKeyDownEsc(event: KeyboardEvent) {
+    if(this.toogleAddListFlag){
+      this.toogleAddListFlag = false;
+    }
+  }
+
+  @HostListener('window:keydown.enter', ['$event'])
+  handleKeyDownEnter(event: KeyboardEvent) {
+    if(this.toogleAddListFlag && this.newTaskList?.nativeElement.value != ''){
+      this.toogleAddListFlag = false;
+      this.taskService.addNewTaskList({name : this.newTaskList.nativeElement.value,  Tasks : [], id: uuidv4()});
+    }
+  }
+
+  toggleNewTaskList($event: MouseEvent) {
+    this.toogleAddListFlag = true;
+    this.newTaskList?.nativeElement.focus();
+  }
 
 }
